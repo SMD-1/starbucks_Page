41cbb97a69ca255510c6e79e11f51da409420044
Danish <skdanish1999@gmail.com>
2021-01-09 23:20:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function imgslider(anything){
+    document.querySelector('.Starbucks').src = anything;
+}
+function backgroundColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
